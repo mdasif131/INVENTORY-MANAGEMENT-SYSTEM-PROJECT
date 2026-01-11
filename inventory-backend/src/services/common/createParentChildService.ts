9a24dcc122b582c974e83daa890c5928558483d0
mdasif131 <asifmdasifchy@gmail.com>
2026-01-11 18:24:09 +0600

    add database rollback transaction

--- inventory-backend/src/services/common/createParentChildService.ts
+++ inventory-backend/src/services/common/createParentChildService.ts
@@ -1,11 +1,11 @@
 
 import type { Document, Model } from 'mongoose';
-import type { Request, Response } from 'express';
+import mongoose from 'mongoose';
 import { AuthRequest, ServiceResponse } from '../../utility/tsTypes';
 
 // Service function with corrected types - supports different Parent and Child types
 export const createParentChildsService = async <
-  TParent extends Document,
+  TParent extends Document ,
   TChild extends Document
 >(
   Requested: AuthRequest,
@@ -13,43 +13,41 @@ export const createParentChildsService = async <
   ChildsModel: Model<TChild>,
   JoinPropertyName: string
 ): Promise<ServiceResponse<any>> => {
+  // Create Transaction Session
+  const session = await mongoose.startSession();
   try {
-    // Parent Creation
+    // Begin Transaction
+    await session.startTransaction();
+
+    // First Database Process
     const parent = Requested.body['parent'];
     parent.userEmail = Requested.user?.email;
-    
-    const parentCreation = await ParentModel.create(parent);
 
-    // Child Creation
-    if (parentCreation._id) {
-    
-      try {
-        const childs = Requested.body['childs'];
-        
-        const updatedChilds = childs.map((element: any) => ({
-          ...element,
-          [JoinPropertyName]: parentCreation._id,
-          userEmail: Requested.user?.email
-        }));
-        
-        // Insert childs and get full documents
-        const childsCreation = await ChildsModel.insertMany(updatedChilds);
-        
-        return {
-          status: 'success',
-          parent: parentCreation,
-          childs: childsCreation,
-        };
-      } catch (error) {
-        console.error('Child creation error:', error);
-        await ParentModel.deleteOne({ _id: parentCreation._id });
-        return { status: 'fail', message: 'Child Creation Failed', error };
-      }
-    } else {
-      return { status: 'fail', message: 'Parent Creation Failed' };
-    }
+    const parentCreation = await ParentModel.create([parent], { session });
+
+    // Second Database Process
+    const childs = Requested.body['childs'];
+    const updatedChilds = childs.map((element: any) => ({
+      ...element,
+      [JoinPropertyName]: parentCreation[0]._id,
+      userEmail: Requested.user?.email,
+    }));
+    const childsCreation = await ChildsModel.insertMany(updatedChilds, {
+      session,
+    });
+
+    // Transaction success
+    await session.commitTransaction();
+    session.endSession();
+    return {
+      status: 'success',
+      parent: parentCreation,
+      childs: childsCreation,
+    };
   } catch (error) {
-    console.error('Parent creation error:', error);
+    // Roll Back Transaction if Fail
+    await session.abortTransaction();
+    session.endSession();
     return { status: 'fail', message: 'Internal Server Error', error };
   }
 };
